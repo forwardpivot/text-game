cd74dffcfbc811fc16c43526ee2583afc56d2b81
Taby <forwardpivot@proton.me>
2022-07-02 12:10:31 -0500

    Got the map area set up, and have an input that console.logs the entered text

---
+++ script.js
@@ -0,0 +1,6 @@
+const destination = document.querySelector('#destination');
+const areaSubmit = document.querySelector('#areaSubmit');
+
+areaSubmit.addEventListener('click', (e) => {
+    console.log(destination.value);
+})
