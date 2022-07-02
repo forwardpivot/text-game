5d67443a1dd9788dd8dd5edadde70c727123276d
Taby <forwardpivot@proton.me>
2022-07-02 12:36:00 -0500

    I added a function that changes the the border color, and the background color. It doesn't clear the others if any toher value is entered though. Also I think its best if only the background color is changed the border is only noticable if I increase the width of it, and i want them all to look the same

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const destination = document.querySelector('#destination');
 const areaSubmit = document.querySelector('#areaSubmit');
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
 
-areaSubmit.addEventListener('click', (e) => {
-    console.log(destination.value);
-})
+areaSubmit.addEventListener('click', changeArea);
+
+
+function changeArea (e) {
+    switch(destination.value) {
+        case 'box1': 
+        box1.style.border = "1px solid green";
+        box1.style.backgroundColor = "grey";
+        console.log(destination.value);
+        break;
+        case 'box2':
+            box2.style.border = "1px solid green";
+            box2.style.backgroundColor = "grey";
+            console.log(destination.value);
+            break;
+        case 'box3':
+            box3.style.border = "1px solid green";
+            box3.style.backgroundColor = "grey";
+            console.log(destination.value);
+            break;
+    }
+}
