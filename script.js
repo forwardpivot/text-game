95483a3afa0b5e2951cc1e72223603291913251b
Taby <forwardpivot@proton.me>
2022-07-03 09:35:00 -0500

    I got it working. I had to use querySelectorAll to get all the .area. I tried using a for in loop, but it didn't work. I would say area.style was null, but using a forEach worked. idk why

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const destination = document.querySelector('#destination');
 const areaSubmit = document.querySelector('#areaSubmit');
-const area = document.querySelector('.area');
+let area = document.querySelectorAll('.area');
 const box1 = document.querySelector('.box1');
 const box2 = document.querySelector('.box2');
 const box3 = document.querySelector('.box3');
@@ -9,7 +9,11 @@ areaSubmit.addEventListener('click', changeArea);
 
 
 function changeArea (e) {
-    area.style.backgroundColor = "white";
+    area.forEach(function(elem) {
+        elem.style.backgroundColor = "white";
+        console.log(elem);
+    });
+
     switch(destination.value) {
         case 'box1': 
         box1.style.backgroundColor = "green";
