060de647ca40bd0e52d2cb7fa2b84975ffcaefc1
Taby <forwardpivot@proton.me>
2022-07-04 10:56:58 -0500

    Messing with grid

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const destination = document.querySelector('#destination');
 const areaSubmit = document.querySelector('#areaSubmit');
 const map = document.querySelector(".map");
 const storyBoard = document.querySelector('#storyBoard');
-let area = document.querySelectorAll('.area');
+
 const box1 = document.querySelector('.box1');
 const box2 = document.querySelector('.box2');
 const box3 = document.querySelector('.box3');
+const box4 = document.querySelector('.box4');
 
 areaSubmit.addEventListener('click', changeArea);
 box1.addEventListener('click', changeArea);
 box2.addEventListener('click', changeArea);
+box3.addEventListener('click', changeArea);
+box4.addEventListener('click', changeArea);
+
 
 
 function changeArea (e) {
+    let area = document.querySelectorAll('.area');
     area.forEach(function(elem) {
         elem.style.backgroundColor = "white";
     });
@@ -23,8 +28,7 @@ function changeArea (e) {
     const MapTile = (name, description,) => {
         const mapTile = name
         const lookAround = () => storyBoard.textContent = description;
-        const changeBorder = () => name.style.border = "5px solid green";
-        return {name, description, lookAround, changeBorder};
+        return {name, description, lookAround};
     }
 
     const box1Look = MapTile(box1, "this is the first box");
